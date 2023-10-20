91a773dadb7bdf40e15f5f63eee6694db928165a
jannand09 <138733341+jannand09@users.noreply.github.com>
2023-10-19 22:24:55 -0400

    Update DSE6003_final_project.py

--- DSE6003_final_project.py
+++ DSE6003_final_project.py
@@ -29,20 +29,76 @@ def de_id_age(value):
     
 
 def de_id_income(value):
-    if value <= 11:
-        return "0-11000"
-    elif value > 11 and value <= 44:
-        return "11001-44000"
-    elif value > 44 and value <= 95:
-        return "44001-95000"
-    elif value > 95 and value <= 182:
-        return "95001-182000"
-    elif value > 182 and value <= 231:
-        return "182001-231000"
-    elif value > 231 and value <= 578:
-        return "231001-578000"
-    elif value > 578:
-        return "578001+"
+    if value <= 30:
+        return "0-30000"
+    elif value > 30 and value <= 80:
+        return "30001-80000"
+    elif value > 80 and value <= 120:
+        return "80001-120000"
+    elif value > 120:
+        return "120001+"
+    
+def de_id_education(value):
+    if value < 11:
+        return "0-11"
+    elif value > 11 and value <= 15:
+        return "12-15"
+    elif value > 15 and value <= 19:
+        return "16-19"
+    elif value > 19:
+        return "20-23"
+
+def de_id_region(value):
+    if value >= 1 and value <=3:
+        return "1-3"
+    elif value > 3:
+        return "4-5"
 
 quasi_data['age_de_id'] = quasi_data['Age'].map(de_id_age)
 quasi_data['income_de_id'] = quasi_data['HouseholdIncome'].map(de_id_income)
+quasi_data['edu_de_id'] = quasi_data['EducationYears'].map(de_id_education)
+quasi_data['region_de_id'] = quasi_data['Region'].map(de_id_region)
+
+# Probability of attempt based on Verizon DBIR - 83% are from external adversaries
+pr_attempt = 1 - 0.83
+
+merr_customers = 6000
+us_pop = 335600000
+
+# Probability of an analyst knowing someone in the data set
+pr_acquaintance = 1 - ((1-(merr_customers/us_pop))**150)
+
+# Probability of a data breach is 85% for small to mid-sized companies
+pr_breach = 0.85
+
+eq_classes = quasi_data.groupby(['age_de_id', 'Gender', 'region_de_id', 'edu_de_id', 'income_de_id']).size().reset_index()
+eq_classes.rename(columns = {0: "count"}, inplace=True)
+eq_classes['pr_re_id'] = 1 / eq_classes['count']
+
+# Calculate Pr(re-id) for all four scenarios
+eq_classes['scenario_1'] = pr_attempt * eq_classes['pr_re_id']
+eq_classes['scenario_2'] = pr_acquaintance * eq_classes['pr_re_id']
+eq_classes['scenario_3'] = pr_breach * eq_classes['pr_re_id']
+eq_classes['scenario_4'] = 1.00 * eq_classes['pr_re_id']
+
+def determine_risk_range(value):
+    if value < 0.05:
+        return "<5%"
+    elif value >= 0.05 and value < 0.10:
+        return "<10%"
+    elif value >= 0.10 and value < 0.20:
+        return "<20%"
+    elif value >= 0.20 and value < 0.33:
+        return "<33%"
+    elif value >= 0.33 and value < 0.5:
+        return "<50%"
+    elif value >= 0.5:
+        return ">50%"
+
+
+eq_classes['s1.risk'] = eq_classes['scenario_1'].map(determine_risk_range)
+eq_classes['s2.risk'] = eq_classes['scenario_2'].map(determine_risk_range)
+eq_classes['s3.risk'] = eq_classes['scenario_3'].map(determine_risk_range)
+eq_classes['s4.risk'] = eq_classes['scenario_4'].map(determine_risk_range)
+
+eq_classes.groupby('s1.risk').size()
